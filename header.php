6e845827fd7a056ff2ba51e231f7109e99a0d868
西原蒼樹 <soki7743291@gmail.com>
2020-11-03 12:38:38 +0900

    no message

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="description" content="<?php bloginfo( 'description' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <link rel="shortcut icon" href="http://hamburgerwp.local/wp-content/uploads/2020/11/favicon-1.png">
   <?php wp_head(); ?> 
   </head>
 
